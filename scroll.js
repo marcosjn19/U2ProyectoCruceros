7c181cca387f76028baefc82b5ad5d9e5de13d82
Marcos J <127040895+marcosjn19@users.noreply.github.com>
2023-11-16 19:54:34 -0600

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,22 +1,10 @@
-if (document.readyState === 'complete') {
-    // La página ya se ha cargado completamente
-    inicializarScrollReveal();
-} else {
-    // Esperar a que la página se cargue completamente
-    document.onreadystatechange = function () {
-        if (document.readyState === 'complete') {
-            // La página ahora está completamente cargada
-            inicializarScrollReveal();
-        }
-    };
-}
-
 function getSlide (delay, origen, distancia, reset) {
     return deslizar = {
         delay:  delay,
         origin: origen,
         distance: distancia,
-        reset: reset
+        reset: reset,
+        mobile: false
     }
 }
 
@@ -26,13 +14,11 @@ function getSlideGroup ( delay, origen, distancia, intervalo, reset ) {
         origin: origen,
         distance: distancia,
         interval: intervalo,
-        reset: reset
+        reset: reset,
+        mobile: false
     }
 }
 
-window.onload = function () {
-    // Inicializar o reiniciar ScrollReveal después de que la página esté completamente cargada
-    ScrollReveal().reveal('.contenido-experiencias h2, .contenido-experiencias p, button', getSlideGroup(100,'right','100%',200, true));
-    ScrollReveal().reveal('.mensaje p', getSlideGroup(500,'left','200%', 200, false));
-    ScrollReveal().reveal('.contenedor-promociones .promocion', getSlideGroup(100, 'right', '150%', 200, true ));
-};
+ScrollReveal().reveal('.contenido-experiencias h2, .contenido-experiencias p, button', getSlideGroup(100,'right','100%',200, true));
+ScrollReveal().reveal('.mensaje p', getSlideGroup(500,'left','200%', 200, false));
+ScrollReveal().reveal('.contenedor-promociones .promocion', getSlideGroup(100, 'right', '150%', 200, true ));
